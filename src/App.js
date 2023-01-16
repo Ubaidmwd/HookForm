95af9f490e3eaddff054ddf4585ed686e1454cd0
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-01-15 21:17:17 -0800

    adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Button from '@mui/material/Button';
+import { Container, width } from '@mui/system';
+import TextField from '@mui/material/TextField';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <br /><br /><br />
+        
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+        <br /><br /><br />
+        <TextField id="standard-basic" label="Standard" variant="standard"  />
+        <br /><br /><br />
+        <TextField id="standard-basic" label="Standard" variant="standard"  />
+        <br /><br /><br />
+        <TextField id="standard-basic" label="Standard" variant="standard"  />
+        <br /><br /><br />
+
+        <Button variant="contained">Hello World</Button>
+       
+      </Container>
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
